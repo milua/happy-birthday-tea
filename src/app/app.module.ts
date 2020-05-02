0eaa3dee0f23cf324bc58520fc8056734c87ddc5
Isabella Tran <tran.isabella9393@gmail.com>
2020-05-02 10:40:24 +0200

    added dialog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DialogModule } from './dialog/dialog.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        DialogModule,
+        NgbModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
