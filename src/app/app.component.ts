dcccfa84050e046cdf098d44a7c53823facc062e
Isabella Tran <tran.isabella93@gmail.com>
2020-05-03 11:43:09 +0200

    add kitchen dialogues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DialogueText } from './material/dialogue-text';
 import { TimerService } from './timer/services/timer.service';
+import { ClickActions } from './scene/click-actions';
+import { Items } from './material/items';
 
 @Component({
   selector: 'app-root',
@@ -8,35 +10,68 @@ import { TimerService } from './timer/services/timer.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
   dialogue: DialogueText;
   isDialogOpen: boolean = true;
   showLogo: boolean = false;
   showKitchen: boolean = false;
-  private allDialogues: DialogueText[];
+  inventory: Items[] = [];
 
   constructor(private timerService: TimerService) {
     this.timerService.timeRunOut.subscribe(() => this.playNextScene());
   }
 
   ngOnInit(): void {
-    this.allDialogues = [
-      DialogueText.INTRO
-    ];
-    this.dialogue = this.allDialogues[0];
+    this.dialogue = DialogueText.INTRO;
   }
 
   onDialogEnded(): void {
     this.isDialogOpen = false;
-    if(this.dialogue === DialogueText.INTRO) {
+    if (this.dialogue === DialogueText.INTRO) {
       this.showLogo = true;
       this.timerService.timeLeft = 10;
       this.timerService.startTimer();
     }
   }
 
+  openKitchenDialog($event: ClickActions): void {
+    switch ($event) {
+      case ClickActions.KITCHEN_CUPBOARD:
+        break;
+      case ClickActions.KITCHEN_WINDOW:
+        break;
+      case ClickActions.KITCHEN_CORN:
+        this.dialogue = [DialogueText.KITCHEN_CORN[this.getRandomTextIndex(DialogueText.KITCHEN_CORN.length)]];
+        this.isDialogOpen = true;
+        break;
+      case ClickActions.KITCHEN_POT:
+        this.dialogue = [DialogueText.KITCHEN_POT[this.getRandomTextIndex(DialogueText.KITCHEN_POT.length)]];
+        this.isDialogOpen = true;
+        break;
+      case ClickActions.KITCHEN_ELASTICS:
+        if (this.inventory.includes(Items.ELASTICS)) {
+          this.dialogue = [DialogueText.KITCHEN_ELASTICS[1]];
+        } else {
+          this.dialogue = [DialogueText.KITCHEN_ELASTICS[0]];
+          this.inventory.push(Items.ELASTICS);
+          // TODO Notification
+        }
+        this.isDialogOpen = true;
+        break;
+      case ClickActions.KITCHEN_COOKIES:
+        this.dialogue = [DialogueText.KITCHEN_COOKIES[this.getRandomTextIndex(DialogueText.KITCHEN_COOKIES.length)]];
+        this.isDialogOpen = true;
+        break;
+      case ClickActions.KITCHEN_DOOR:
+        break;
+    }
+  }
+
   private playNextScene() {
     this.showLogo = false;
     this.showKitchen = true;
+  }
+
+  private getRandomTextIndex(textLength: number) {
+    return Math.round(Math.random() * (textLength - 1));
   }
 }
--- src/app/material/dialogue-text.ts
+++ src/app/material/dialogue-text.ts
@@ -4,4 +4,12 @@ export class DialogueText {
     'Although the distance between us became as far as it could be, I still felt very close to her.',
     'Today was her birthday.',
     'I got up to make some tea, as usual. I went downstairs into the kitchen but something felt different.'];
+
+  static KITCHEN_CORN: string[] = ['This is just some corn.', '...', 'I don\'t need this.'];
+  static KITCHEN_POT: string[] = ['A pot with water.', 'Not very interesting...'];
+  static KITCHEN_ELASTICS: string[] = ['Always good to have some elastics with you.', 'I already have enough of them.'];
+  static KITCHEN_COOKIES: string[] = ['I shouldn\'t but...*crunch* *crunch*', 'Cookies!'];
+  static KITCHEN_DOOR: string[] = ['Should I go outside?'];
+
+
 }
